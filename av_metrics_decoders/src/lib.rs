e84018a5c3ae641cf84ab3659ce8f7cea364e255
Luni-4 <luni-4@hotmail.it>
2021-07-22 15:18:13 +0200

    Add a feature to build ffmpeg statically (#292)

--- av_metrics_decoders/src/lib.rs
+++ av_metrics_decoders/src/lib.rs
@@ -12,10 +12,10 @@ mod y4m;
 #[cfg(feature = "y4m")]
 pub use crate::y4m::Y4MDecoder;
 
-#[cfg(feature = "ffmpeg")]
+#[cfg(any(feature = "ffmpeg", feature = "ffmpeg_static"))]
 mod ffmpeg;
 
-#[cfg(feature = "ffmpeg")]
+#[cfg(any(feature = "ffmpeg", feature = "ffmpeg_static"))]
 pub use crate::ffmpeg::FfmpegDecoder;
 
 pub use av_metrics::video::decode::Decoder;
